11cb50a6ba70a7a862283d86123623e124d92cd0
jwathika <wathika02@gmail.com>
2024-04-09 22:06:06 +0300

    ip&&docstrings

--- ip_app.py
+++ ip_app.py
@@ -1,34 +1,26 @@
-# imports to use
+"""
+Will get users IP then pass it as a parameter to wakatime API
+User will get results based on IP (Country Code)
+https://www.myip.com/api-docs/
+"""
+
 from flask import Flask, jsonify, request, redirect, url_for
 from flask_cors import CORS
 import requests
 import json
 import time
 
-
-"""
-Main API can be found at:
-https://wakatime.com/api/v1/leaders
-https://wakatime.com/api/v1/leaders?country_code=KE
-https://wakatime.com/developers/
-"""
-
 app = Flask(__name__)
-# solved cors issues especially in 127.0.0.1
 CORS(app)
 
-# Set the maximum number of requests allowed per hour (60) to prevent bots
-MAX_REQUESTS_PER_HOUR = 60
-# initial count
+# Set the maximum number of requests allowed per hour
+MAX_REQUESTS_PER_HOUR = 50
 request_count = 0
-# check when last reset was done
 last_reset_time = time.time()
 
 
-# url and method allowed
 @app.route("/api/leaders", methods=["GET"])
 def get_top_leaders():
-    # get && define global vars
     global request_count, last_reset_time
     current_time = time.time()
 
@@ -37,7 +29,6 @@ def get_top_leaders():
         request_count = 0
         last_reset_time = current_time
 
-    # once limit is reached show error or signup page to proceed
     if request_count >= MAX_REQUESTS_PER_HOUR:
         return (
             jsonify(
@@ -46,74 +37,66 @@ def get_top_leaders():
                 }
             ),
             429,
-        )
-        # Return 429 for Too Many Requests
+        )  # Return 429 for Too Many Requests
     else:
-        # if not reached, continuing increasing till limit
         request_count += 1
 
-    # main url
     url = "https://wakatime.com/api/v1/leaders"
+    # api_key = "your_api_key_here"
 
-    # add header options if need be
-    # headers = {}
+    # headers = {"Authorization": f"Bearer {api_key}"}
 
-    # paramaters to pass to URL
     params = {"limit": 10}
-    # Not working currently
 
-    # Check if country_code is provided as a query parameter
-    country_code = request.args.get("country_code")
-    if country_code:
-        params["country_code"] = country_code
+    # Get user's IP information
+    ip_info_response = requests.get("https://api.myip.com")
+    if ip_info_response.status_code == 200:
+        ip_info = ip_info_response.json()
+        print(ip_info)
+        country_code = ip_info.get("cc")
 
-    # fetch the url response together with parameters
+        if country_code:
+            params["country_code"] = country_code
+
     response = requests.get(url, params=params)
-    # if response is okay(200)
+
     if response.status_code == 200:
-        # jsonify the response
         data = response.json()["data"]
         stripped_data = []
-        """
-        Get the top 15 leaders worldwide or per country
-        filter details to only leave city, username && country(includes city)
-        if no data for user leave empty
-        Also get top 3 languages and time coded
-        If possible might include languages mascot
-        """
-        for leader in data[:15]:
+
+        for leader in data[:15]:  # Get the top 15 leaders
             if "user" in leader:
                 user = leader["user"]
-                city = user.get("city")  # Use .get() to safely retrieve the value
+                city = user.get("city")
                 if city:
                     user_title = city.get("title", "")
                     country_code = city.get("country_code", "")
+                else:
+                    user_title = ""
+                    country_code = ""
             else:
                 user_title = ""
                 country_code = ""
 
             if "running_total" in leader:
                 running_total = leader["running_total"]
-                running_daily = running_total["human_readable_daily_average"]
+                running_total_seconds = running_total["total_seconds"]
                 running_total_human_readable = running_total["human_readable_total"]
                 top_3_languages = [
                     {"name": lang["name"], "total_seconds": lang["total_seconds"]}
                     for lang in running_total["languages"][:3]
                 ]
             else:
-                running_daily = 0
+                running_total_seconds = 0
                 running_total_human_readable = ""
                 top_3_languages = []
 
-            """
-            Now show the final info
-            """
             stripped_leader = {
                 "rank": leader["rank"],
                 "username": user["username"],
                 "city": user_title,
                 "country_code": country_code,
-                "running_daily": running_daily,
+                "running_total_seconds": running_total_seconds,
                 "running_total_human_readable": running_total_human_readable,
                 "top_3_languages": top_3_languages,
             }
@@ -121,9 +104,6 @@ def get_top_leaders():
 
         return jsonify(stripped_data)
     else:
-        """
-        If data fails to fetch, show an error and the reason(currently on console)
-        """
         return (
             jsonify(
                 {
@@ -134,13 +114,12 @@ def get_top_leaders():
         )
 
 
-# set default path and redirect if user goes to unverified URL
+# Catch-all route to redirect to main URL for undefined routes
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def catch_all(path):
     return redirect(url_for("get_top_leaders"))
 
 
-# finally run the app only when called by name
 if __name__ == "__main__":
     app.run(debug=True)
