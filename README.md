dad8b416a9e1b9536104fedbef58d03e0142a9a6
wathika-eng <wathika02@gmail.com>
2024-08-11 14:43:29 +0300

    chore: Update environment variables and dependencies

--- README.md
+++ README.md
@@ -3,36 +3,50 @@
 Ensure you have python3, pip and virtualenv installed in your OS, then:
 
 Fork/clone to your PC:
+
 ```
-git clone https://github.com/wathika-eng/wakatime_flask_api.git
+git clone https://github.com/wathika-eng/wakatime_flask_api.git --depth 1
 cd wakatime_flask_api
 ```
 
 Create and activate a virtual environment
+
 ```
-virtualenv venv
+virtualenv .venv
 ```
 
 Linux:
+
 ```
-source venv/bin/activate
+source .venv/bin/activate
 ```
 
 Windows:
+
 ```
-source venv/scripts/activate
+source .venv/scripts/activate
 ```
 
 Install all requirements needed:
+
 ```
 pip install -r requirements.txt
 ```
 
+Copy the `.env.example` file to `.env` and fill in the required fields.
+
+```
+cp .env.example .env
+```
+
 Run main app:
+
 ```
 flask run
 ```
+
 or
+
 ```
 python ip_app.py
 ```
--- app.py
+++ app.py
@@ -14,30 +14,26 @@ https://wakatime.com/developers/
 """
 
 app = Flask(__name__)
-# solved cors issues especially in 127.0.0.1
+
 CORS(app)
 
-# Set the maximum number of requests allowed per hour (60) to prevent bots
+
 MAX_REQUESTS_PER_HOUR = 60
-# initial count
+
 request_count = 0
-# check when last reset was done
+
 last_reset_time = time.time()
 
 
-# url and method allowed
 @app.route("/api/leaders", methods=["GET"])
 def get_top_leaders():
-    # get && define global vars
     global request_count, last_reset_time
     current_time = time.time()
 
-    # Reset request count if an hour has passed since the last reset
     if current_time - last_reset_time > 3600:
         request_count = 0
         last_reset_time = current_time
 
-    # once limit is reached show error or signup page to proceed
     if request_count >= MAX_REQUESTS_PER_HOUR:
         return (
             jsonify(
@@ -47,44 +43,33 @@ def get_top_leaders():
             ),
             429,
         )
-        # Return 429 for Too Many Requests
     else:
-        # if not reached, continuing increasing till limit
+
         request_count += 1
 
-    # main url
     url = "https://wakatime.com/api/v1/leaders"
 
-    # add header options if need be
-    # headers = {}
+    page = int(request.args.get("page", 1))
+    per_page = int(request.args.get("per_page", 10))
 
-    # paramaters to pass to URL
-    params = {"limit": 10}
-    # Not working currently
+    offset = (page - 1) * per_page
 
-    # Check if country_code is provided as a query parameter
+    params = {"limit": per_page, "offset": offset}
+
     country_code = request.args.get("country_code")
     if country_code:
         params["country_code"] = country_code
 
-    # fetch the url response together with parameters
     response = requests.get(url, params=params)
-    # if response is okay(200)
+
     if response.status_code == 200:
-        # jsonify the response
         data = response.json()["data"]
         stripped_data = []
-        """
-        Get the top 15 leaders worldwide or per country
-        filter details to only leave city, username && country(includes city)
-        if no data for user leave empty
-        Also get top 3 languages and time coded
-        If possible might include languages mascot
-        """
-        for leader in data[:15]:
+
+        for leader in data:
             if "user" in leader:
                 user = leader["user"]
-                city = user.get("city")  # Use .get() to safely retrieve the value
+                city = user.get("city")
                 if city:
                     user_title = city.get("title", "")
                     country_code = city.get("country_code", "")
@@ -105,9 +90,6 @@ def get_top_leaders():
                 running_total_human_readable = ""
                 top_3_languages = []
 
-            """
-            Now show the final info
-            """
             stripped_leader = {
                 "rank": leader["rank"],
                 "username": user["username"],
@@ -119,11 +101,10 @@ def get_top_leaders():
             }
             stripped_data.append(stripped_leader)
 
-        return jsonify(stripped_data)
+        # Include pagination metadata in the response
+        return jsonify({"page": page, "per_page": per_page, "data": stripped_data})
+
     else:
-        """
-        If data fails to fetch, show an error and the reason(currently on console)
-        """
         return (
             jsonify(
                 {
@@ -134,13 +115,11 @@ def get_top_leaders():
         )
 
 
-# set default path and redirect if user goes to unverified URL
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def catch_all(path):
     return redirect(url_for("get_top_leaders"))
 
 
-# finally run the app only when called by name
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, load_dotenv=True)
